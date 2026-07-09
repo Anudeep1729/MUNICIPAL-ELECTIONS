76cc1c8982c4c9ca0b8f4b2bc3f1c636c0ad4480
Sangem Anudeep <139117538+Anudeep1729@users.noreply.github.com>
2026-07-09 20:05:05 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
   "./What'sapp-Image.jpeg"
 ];
 
